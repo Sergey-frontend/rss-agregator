c30c0da4f67b2dccfbcb079f3c52143ec241e5cc
Sergey T <tolstikovsn@gmail.com>
2023-03-25 13:25:17 +0500

    f

--- src/app.js
+++ src/app.js
@@ -1,32 +1,42 @@
 import * as yup from 'yup';
-import watch from './view';
 import i18next from 'i18next';
+import watch from './view.js';
+import ru from './locales/ru.js'
 
-const elements = {
-  input: document.querySelector('#url-input'),
-  feedback: document.querySelector('.feedback'),
-  form: document.querySelector('form'),
-  button: document.querySelector('button'),
-};
+const app = async () => {
 
-const state = {
-  form: {
-    status: 'filling',
-    error: null,
-  },
-  urls: [],
-};
-
-const watchedState = watch(state, elements);
-
-const validateUrl = (url, urls) => yup
-  .string()
-  .url(i18next.t('errors.invalidUrl'))
-  .notOneOf(urls, i18next.t('errors.alreadyLoaded'))
-  .required()
-  .validate(url);
-
-const app = () => {
+  await i18next.init({
+    lng: 'ru',
+    debug: true,
+    resources: {
+      ru,
+    },
+  });
+  
+  const elements = {
+    input: document.querySelector('#url-input'),
+    feedback: document.querySelector('.feedback'),
+    form: document.querySelector('form'),
+    button: document.querySelector('button'),
+  };
+  
+  const state = {
+    form: {
+      status: 'filling',
+      error: null,
+    },
+    urls: [],
+  };
+  
+  const watchedState = watch(state, elements);
+  
+  const validateUrl = (url, urls) => yup
+    .string()
+    .url(i18next.t('errors.invalidUrl'))
+    .notOneOf(urls, i18next.t('errors.alreadyLoaded'))
+    .required()
+    .validate(url);
+  
   elements.form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     const formData = new FormData(evt.target);
